357dd61c374f3f1781d71d4d41d44284033b29db
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-05-27 19:22:15 +0000

    test: changes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'jest-expo',
-    // Esto es vital para que Jest no explote con los módulos de Expo que necesitan ser transpilados
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
     ],
-};
+  };
