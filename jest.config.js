b19b0d281c2d3f24db754f2056b59c46aefe965c
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-05-28 20:01:49 +0000

    test: jestConfig

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,12 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
-  // Mapeamos los alias y los mocks de archivos estáticos
   moduleNameMapper: {
-    // Si la imagen está en la raíz:
-    '^@/assets/(.*)$': '<rootDir>/assets/$1', 
-    // Si tuvieras otros alias hacia src:
+    '^@/assets/(.*)$': '<rootDir>/assets/$1',
     '^@/(.*)$': '<rootDir>/src/$1',
-    // Y finalmente, el mock para CUALQUIER imagen (png, jpg, jpeg, gif, svg)
+    // AGREGAMOS CSS AQUÍ:
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/fileMock.js',
+    // Mantenemos tus assets actuales
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
 };
