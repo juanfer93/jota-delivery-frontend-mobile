b512860b011e9c1f027131049f559551594fb247
dabestjuan1993 <dabestjuan1993@gmail.com>
2026-05-28 20:09:13 +0000

    test: jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,19 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
   moduleNameMapper: {
+    // 1. PRIMERO: Captura los imports de CSS con alias
+    '^@/(.*)\\.css$': '<rootDir>/__mocks__/fileMock.js',
+    
+    // 2. DESPUÉS: Tus otros alias de assets
     '^@/assets/(.*)$': '<rootDir>/assets/$1',
+    
+    // 3. DESPUÉS: Tu alias general (no atrapará los CSS por el orden)
     '^@/(.*)$': '<rootDir>/src/$1',
-    // AGREGAMOS CSS AQUÍ:
+    
+    // 4. Mapeo para CSS estándar
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/fileMock.js',
-    // Mantenemos tus assets actuales
+    
+    // Mantenemos los assets
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
 };
